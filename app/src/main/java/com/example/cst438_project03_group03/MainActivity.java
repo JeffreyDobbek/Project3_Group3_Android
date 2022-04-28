47035db9d179b3b111a44a5e55806911aed696d9
JeffreyDobbek <jdobbek@csumb.edu>
2022-04-28 12:42:09 -0700

    Added an activity for posting and for viewing posts. I also connected the main activity to them. I still need to connect these things to the database.

--- app/src/main/java/com/example/cst438_project03_group03/MainActivity.java
+++ app/src/main/java/com/example/cst438_project03_group03/MainActivity.java
@@ -8,10 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.cst438_project03_group03.R;
+import com.example.cst438_project03_group03.database.Post;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logoutButton;
+    private Button makePostButton;
+    private Button viewPostsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logoutButton = findViewById(R.id.logOutButton);
+        makePostButton = findViewById(R.id.makePostButton);
+        viewPostsButton = findViewById(R.id.viewPostsButton);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        makePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+                startActivity(intent);
+            }
+        });
+        viewPostsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PostActivity.class);
                 startActivity(intent);
             }
         });
