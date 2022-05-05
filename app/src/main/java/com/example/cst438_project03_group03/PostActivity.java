56d650ac11ded2e13707da073f75aa6a5348dbd2
JeffreyDobbek <jdobbek@csumb.edu>
2022-05-05 12:39:43 -0700

    Added code to post activity to retrieve a photo from the phone and get an imagur link. Not quite working...will continue to fiddle with it.

--- app/src/main/java/com/example/cst438_project03_group03/PostActivity.java
+++ app/src/main/java/com/example/cst438_project03_group03/PostActivity.java
@@ -1,14 +1,113 @@
 package com.example.cst438_project03_group03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.cst438_project03_group03.databinding.FragmentSecondCreateAccountBinding;
+import com.example.cst438_project03_group03.fragments.SecondCreateAccountFragment;
+import com.example.cst438_project03_group03.models.CreateAccountResult;
+import com.example.cst438_project03_group03.models.ImgurResponse;
+import com.example.cst438_project03_group03.models.ImgurUpload;
+import com.example.cst438_project03_group03.models.UserInfo;
+import com.example.cst438_project03_group03.viewmodels.ImageViewModel;
+import com.example.cst438_project03_group03.viewmodels.UserViewModel;
+
+import java.util.List;
+
 
 public class PostActivity extends AppCompatActivity {
+
+    private String mEmail;
+    private String mPassword;
+    private String mConfirmPassword;
+    private String mUsername;
+    private String mName;
+    private String mPic;
+
+    private UserInfo mUser;
+    private List<UserInfo> mUsers;
+
+    private UserViewModel mUserViewModel;
+    private ImageViewModel mImageViewModel;
+    private ImgurUpload mImgurUpload = new ImgurUpload();
+
+    private Button mUploadPictureButton;
+
+    public static PostActivity newInstance() {
+        return new PostActivity();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            ImageView imageView = findViewById(R.id.postImageView);
+            imageView.setImageURI(selectedImage);
+        }
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        mUserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        mUserViewModel.init();
+
+        mImageViewModel = new ViewModelProvider(this).get(ImageViewModel.class);
+        mImageViewModel.init();
+
+
+
+        mImageViewModel.getImgurResponseLiveData().observe(getViewLifecycleOwner(), new Observer<ImgurResponse>() {
+            @Override
+            public void onChanged(ImgurResponse imgurResponse) {
+                if (imgurResponse != null) {
+                    mPic = imgurResponse.getData().getLink();
+                    mUser = new UserInfo();
+
+                    mUser.setUsername(mUsername);
+                    mUser.setEmail(mEmail);
+                    mUser.setName(mName);
+                    mUser.setPic(mPic);
+                    mUser.setPassword(mPassword);
+
+                    mUserViewModel.createUser(mUser);
+                } else {
+                    Toast.makeText(getContext().getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    Button upload = findViewById(R.id.uploadPhotoButton);
+    upload.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 3);
+        }
+    });
 }
